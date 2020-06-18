e2ace46ef8797fb9f70096f6777f05907b1da198
Carl Syner <casyner@microsoft.com>
2020-06-18 17:33:07 +0100

    Update

--- on-prem.tf
+++ on-prem.tf
@@ -206,6 +206,40 @@ resource "azurerm_virtual_machine" "onprem-dc-vm" {
   }
 }
 
+##########################################################
+## Promote VM to be a Domain Controller
+##########################################################
+
+
+locals { 
+import_command       = "Import-Module ADDSDeployment"
+password_command     = "$password = ConvertTo-SecureString var.password -AsPlainText -Force"
+install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
+configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName contoso.com -DomainNetbiosName contoso -ForestMode 7 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
+exit_code_hack       = "exit 0"
+powershell_command   = "${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.exit_code_hack}"
+#powershell_command  = "Import-Module ADDSDeployment; $password = ConvertTo-SecureString \"Password1234!\" -AsPlainText -Force; Add-WindowsFeature -name ad-domain-services -IncludeManagementTools; Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName contoso.com -DomainNetbiosName contoso -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true; shutdown -r -t 40"
+}
+
+resource "azurerm_virtual_machine_extension" "create-active-directory-forest" {
+  
+  depends_on = [azurerm_virtual_machine.onprem-dc-vm]
+  
+  name                 = "create-active-directory-forest"
+  location             = var.onpremlocation
+  resource_group_name  = "private-endpoint-openhack-onprem-rg"
+  virtual_machine_name = "onprem-dc-vm"
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
+    }
+SETTINGS
+}
+
 #######################################################################
 ## Create Virtual Network Gateway
 #######################################################################
@@ -239,3 +273,4 @@ resource "azurerm_virtual_network_gateway" "onprem-vpn-gateway" {
   depends_on = [azurerm_public_ip.onprem-vpn-gateway-pip]
 
 }
+
